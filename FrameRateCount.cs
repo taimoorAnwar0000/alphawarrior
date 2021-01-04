a6f4b72e0b8ee9c89265e4b62e25de7be44b62ce
taimoorAnwar0000 <57680774+taimoorAnwar0000@users.noreply.github.com>
2021-01-03 22:39:44 -0800

    Add files via upload

---
+++ FrameRateCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FrameRateCount : MonoBehaviour {
+		
+	public Text _text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_text.text = (1 / Time.smoothDeltaTime).ToString();
+	}
+}
